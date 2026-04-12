04616b90b1b29c92cf76ee998138136b6f54ee38
siakinnik <106670383+siakinnik@users.noreply.github.com>
2026-04-12 21:09:33 +0200

    fix: correct variable name and logic in isInOwner utility

--- utils/isInOwner.js
+++ utils/isInOwner.js
@@ -3,7 +3,7 @@
 // ---------------------------------
 
 const isInOwner = (owner, id) => {
-    return owner.indexOf(msg.chat.id) !== -1;
+    return owner.indexOf(id) !== -1;
 };
 
 module.exports = { isInOwner };
